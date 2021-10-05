2c48fa5c50dd63dec8094eed6e8d20a12f5adb56
Jonas Lindell <Carljonaslindell@gmail.com>
2021-10-05 14:39:14 +0200

    added logger

--- WebApp/WebApp/Pages/CreateTodo.cshtml.cs
+++ WebApp/WebApp/Pages/CreateTodo.cshtml.cs
@@ -4,25 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using RestSharp;
 
 namespace WebApp.Pages
 {
     public class CreateTodoModel : PageModel
     {
+        private readonly ILogger<CreateTodoModel> _logger;
+        public CreateTodoModel(ILogger<CreateTodoModel> logger)
+        {
+            _logger = logger;
+        }
         [BindProperty]
         public string Title { get; set; }
         [BindProperty]
         public string Description { get; set; }
         public void OnPost()
         {
-            var todo = new ToDoModel {Title = Title, Description = Description};
-            // var client = new RestClient("https://cosmos-api-jonas.azurewebsites.net/api");
-            // var request = new RestRequest("/Add-New-ToDo?code=syEs2sb8l7DLLXMObmXp3yQa3vn6Yn2igI9T2CRT7cxEavL0ub2eHQ==");
-            var client = new RestClient("https://cosmos-api-rest.azurewebsites.net/api");
-            var request = new RestRequest("/Add-New-ToDo?code=BDiMc3NicpgX4y7xnjKaXcUi/vhnVsO2Ar927PM72mGrjSd7BHiskw==");
-            request.AddJsonBody(todo);
-            var response = client.Post<ToDoModel>(request);
+            try
+            {
+                var todo = new ToDoModel {Title = Title, Description = Description};
+                var client = new RestClient("https://cosmos-api-rest.azurewebsites.net/api");
+                var request = new RestRequest("/Add-New-ToDo?code=BDiMc3NicpgX4y7xnjKaXcUi/vhnVsO2Ar927PM72mGrjSd7BHiskw==");
+                request.AddJsonBody(todo);
+                var response = client.Post<ToDoModel>(request);
+                _logger.LogInformation($"Successfully saved todo: {todo.Title} to db");
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError($"Error exception: {ex}");
+            }
         }
     }
 }
--- WebApp/WebApp/Pages/Todos.cshtml.cs
+++ WebApp/WebApp/Pages/Todos.cshtml.cs
@@ -4,20 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using RestSharp;
 
 namespace WebApp.Pages
 {
     public class TodosModel : PageModel
     {
+        private readonly ILogger<TodosModel> _logger;
+        public TodosModel(ILogger<TodosModel> logger)
+        {
+            _logger = logger;
+        }
         public List<ToDoModel> TodoModels;
         public async Task<IActionResult> OnGet()
         {
-            var client = new  RestClient("https://cosmos-api-rest.azurewebsites.net/api");
-            var request = new RestRequest($"/ToDoGet?code=OS8k7CUuC3yXBm910AeXYaZvGg5SVDmCzVW3gGgiN0Ks3sYK6nN77A==", DataFormat.Json);
+            try
+            {
+                var client = new  RestClient("https://cosmos-api-rest.azurewebsites.net/api");
+                var request = new RestRequest($"/ToDoGet?code=OS8k7CUuC3yXBm910AeXYaZvGg5SVDmCzVW3gGgiN0Ks3sYK6nN77A==", DataFormat.Json);
+                var response = await client.ExecuteAsync<List<ToDoModel>>(request);
+                TodoModels = response.Data;
+                _logger.LogInformation("Successfully retrieved todo's from db.");
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError($"Error exception: {ex}");
+            }
 
-            var response = await client.ExecuteAsync<List<ToDoModel>>(request);
-            TodoModels = response.Data;
             return Page();
         }
     }
